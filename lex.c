c5875898919358035bea5f0719030d084699287c
io12 <io12@users.noreply.github.com>
2017-09-14 14:51:35 -0400

    Moved enum tok and MAX_IDENT_SIZE to langc.h

--- lex.c
+++ lex.c
@@ -20,43 +20,6 @@
 #define LEN(x) (sizeof(x) / sizeof((x)[0]))
 #define IN_RANGE(x, min, max) (x >= min && x <= max)
 #define MAX_STRING_SIZE 1024 // TODO: Make this unlimited
-#define MAX_IDENT_SIZE 512
-
-enum tok {
-	N, A, // N/A (Not applicable) == 0
-
-	IMPURE,
-	IDENT,
-	TYPEDEF,
-
-	TRUE, FALSE,
-	NUM_LITERAL, CHAR_LITERAL, STRING_LITERAL,
-
-	PLUS_PLUS, MINUS_MINUS,
-	PLUS, MINUS, STAR, SLASH, PERCENT,
-	LT, GT, LT_EQ, GT_EQ, EQ_EQ, BANG_EQ,
-	AMP, PIPE, CARET, TILDE, LT_LT, GT_GT,
-	AMP_AMP, PIPE_PIPE, CARET_CARET, BANG,
-	EQ,
-	PLUS_EQ, MINUS_EQ, STAR_EQ, SLASH_EQ, PERCENT_EQ,
-	AMP_EQ, PIPE_EQ, CARET_EQ, LT_LT_EQ, GT_GT_EQ,
-
-	IF, THEN, ELSE, DO, WHILE, FOR, MATCH, COND,
-	BREAK, CONTINUE, DEFER, RETURN,
-
-	U8, U16, U32, U64,
-	I8, I16, I32, I64,
-	F32, F64,
-	BOOL, VOID, CHAR,
-
-	DOT, COLON, SEMICOLON, COMMA,
-
-	OPEN_BRACKET, CLOSE_BRACKET,
-	OPEN_PAREN, CLOSE_PAREN,
-	OPEN_BRACE, CLOSE_BRACE,
-
-	TEOF
-};
 
 static uint16_t lineno = 1;
 static union {
