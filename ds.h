426e052779b8d5a0451ffd126e678a4c28ee75a1
io12 <io12@users.noreply.github.com>
2017-09-21 18:21:49 -0400

    Added hash table function prototypes to ds.h

--- ds.h
+++ ds.h
@@ -12,3 +12,7 @@ typedef struct hash_table HashTable;
 struct hash_table {
 	Vec *data[UINT8_MAX + 1];
 };
+
+HashTable *alloc_hash_table(void);
+void hash_table_set(HashTable *ht, const char *key, void *val);
+void *hash_table_get(HashTable *ht, const char *key);
