af9a64dbee67f07fea56704b20558ffa8a38f5ca
io12 <io12@users.noreply.github.com>
2017-11-23 14:08:34 -0500

    Added -Werror=missing-prototypes to build script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-CFLAGS="-g -O0 -std=c99 -pedantic -Wall -Wextra -Wfatal-errors"
+CFLAGS="-g -O0 -std=c99 -pedantic -Wall -Wextra -Wfatal-errors -Werror=missing-prototypes"
 cc $CFLAGS *.c -o quoftc
