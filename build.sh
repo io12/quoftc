3633b55a1514e2b675a22d347156184a6c6cac85
io12 <io12@users.noreply.github.com>
2017-11-23 17:57:01 -0500

    Added -Werror to build script

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-CFLAGS="-g -O0 -std=c99 -pedantic -Wall -Wextra -Wfatal-errors -Werror=missing-prototypes"
+CFLAGS="-g -O0 -std=c99 -pedantic -Wall -Wextra -Werror -Wfatal-errors \
+	-Werror=missing-prototypes"
 cc $CFLAGS *.c -o quoftc
