2030274f014d287c6af555d7f31198aa72b0a961
io12 <io12@users.noreply.github.com>
2017-12-23 13:48:19 -0500

    Updated build script

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-CFLAGS="-g -O0 -std=c99 -pedantic -Wall -Wextra -Werror -Wfatal-errors \
+cflags="-g -O0 -std=c99 -pedantic -Wall -Wextra -Werror -Wfatal-errors \
 	-Werror=missing-prototypes `llvm-config --cflags`"
-LDFLAGS="`llvm-config --ldflags --libs`"
-cc $CFLAGS $LDFLAGS *.c -o quoftc
+ldflags="`llvm-config --ldflags --libs`"
+args="$cflags $ldflags *.c -o quoftc"
+gcc $args && clang $args
