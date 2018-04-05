92c8cb0b1482822b2905103c2ae697858b169565
io12 <io12@users.noreply.github.com>
2018-04-04 23:33:48 -0400

    Added -Wno-missing-braces to build script so that ALLOC_STRUCT() could fill in fields in struct headers

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-cflags="-g -O0 -std=c99 -pedantic -Wall -Wextra -Werror -Wfatal-errors \
-	-Werror=missing-prototypes `llvm-config --cflags`"
+warning_flags="-Wall -Wextra -Werror -Wfatal-errors -Werror=missing-prototypes \
+	-Wno-missing-braces"
+cflags="-g -O0 -std=c99 -pedantic $warning_flags `llvm-config --cflags`"
 ldflags="`llvm-config --ldflags --libs`"
 args="$cflags $ldflags *.c -o quoftc"
 gcc $args && clang $args && ./run_tests.sh
